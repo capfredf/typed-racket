6b79c5f310b9def984dbec1425df814eb53a6e7d
Fred Fu <moonsolo@gmail.com>
2022-01-15 00:09:18 -0500

    tmp

--- typed-racket-test/unit-tests/typecheck-tests.rkt
+++ typed-racket-test/unit-tests/typecheck-tests.rkt
@@ -244,7 +244,7 @@
     'custom-ret))
 
 (provide tests)
-(gen-test-main)
+;; (gen-test-main)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -5639,3 +5639,53 @@
 
 (provide all-tests)
 (define tests (make-test-suite "typecheck-tests" all-tests))
+
+
+(define total (length all-tests))
+(define workload (make-parameter #f))
+(define n-workers (make-parameter #f))
+
+(define (safe-take lst pos)
+  (cond
+    [(null? lst) lst]
+    [(>= (length lst) pos) (take lst pos)]
+    [else null]))
+
+(define (safe-drop lst pos)
+  (cond
+    [(null? lst) lst]
+    [(>= (length lst) pos) (drop lst pos)]
+    [else null]))
+
+;; (define all-tests (list (make-test-suite "hello" (test-eq? "EQ" 'a 'a))))
+
+(define (start get-ch id workload)
+  (place/context aa
+    ;;(eprintf "aa ~a ~n" (place-channel-get aa))
+    (let loop ()
+      [define a (place-channel-get get-ch)]
+      (if (equal? a 'over) #t
+          (begin (run-test (make-test-suite "hi" (safe-take (safe-drop all-tests (* a workload)) workload)))
+                 (loop))))))
+
+
+(define (prun)
+  (define-values (put-ch get-ch) (place-channel))
+
+  (define workers (build-list (n-workers) (lambda n (start get-ch n (workload)))))
+  (eprintf "....~a ~n" 10)
+  (for/list ([i (in-range (n-workers))])
+    (place-channel-put put-ch i))
+  (eprintf "....~a ~n" 42)
+  (for ([w (in-list workers)])
+    (place-channel-put put-ch 'over))
+  (eprintf "....~a ~n" 24)
+  (for ([w (in-list workers)])
+    (place-wait w)))
+
+(require racket/cmdline)
+(module+ main
+  (command-line #:args (wl)
+                (workload (string->number wl))
+                (n-workers (ceiling (/ total (workload)))))
+  (time (prun)))
