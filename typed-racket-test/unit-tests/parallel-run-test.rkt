883edab837cb62a3a6aedc73aeb4c3b272e1af4f
Fred Fu <moonsolo@gmail.com>
2022-01-14 23:27:12 -0500

    clean up

--- typed-racket-test/unit-tests/parallel-run-test.rkt
+++ typed-racket-test/unit-tests/parallel-run-test.rkt
@@ -1,72 +1,35 @@
 #lang racket/base
-(require rackunit rackunit/text-ui racket/place racket/list syntax/location)
+(require racket/match rackunit rackunit/text-ui racket/place racket/list syntax/location
+         rackunit/private/format)
 
 (require (only-in "typecheck-tests.rkt" [tests tt:tests])
          (only-in "subtype-tests.rkt" [tests st:tests]))
 
-;; (define total (length tt:tests))
-;; (define workload 300)
-;; (define n-workers (ceiling (/ total workload)))
-
-(define (safe-take lst pos)
-  (cond
-    [(null? lst) lst]
-    [(>= (length lst) pos) (take lst pos)]
-    [else null]))
-
-(define (safe-drop lst pos)
-  (cond
-    [(null? lst) lst]
-    [(>= (length lst) pos) (drop lst pos)]
-    [else null]))
-
-;; (define all-tests (list (make-test-suite "hello" (test-eq? "EQ" 'a 'a))))
-#;
-(define (start get-ch id)
-  (place/context aa
-    ;;(eprintf "aa ~a ~n" (place-channel-get aa))
-    (let loop ()
-      [define a (place-channel-get get-ch)]
-      (if (equal? a 'over) #t
-          (begin (run-test (make-test-suite "hi" (safe-take (safe-drop all-tests (* a workload)) workload)))
-                 (loop))))))
 
 (require racket/runtime-path)
 
+(define state (make-hash))
 (define-runtime-path src-dir ".")
 (define (start2 get-ch id)
   (place/context aa
-    ;;(eprintf "aa ~a ~n" (place-channel-get aa))
-    (let loop ()
+    (let loop ([success 0]
+               [failure 0]
+               [err 0])
       [define a (place-channel-get get-ch)]
-      (if (equal? a 'over) #t
-          (begin (run-tests (dynamic-require (build-path src-dir a) 'tests))
-                 (loop))))))
-
-(define (start2-1 name)
-  (place/context aa
-    ;;(eprintf "aa ~a ~n" (place-channel-get aa))
-    (run-tests (dynamic-require (build-path src-dir name) 'tests))
-    #;
-    (let loop ()
-      [define a (place-channel-get get-ch)]
-      (if (equal? a 'over) #t
-          (begin
-                 (loop))))))
-#;
-(define (prun)
-  (define-values (put-ch get-ch) (place-channel))
-
-  (define workers (build-list n-workers (lambda n (start get-ch n))))
-  (eprintf "....~a ~n" 10)
-  (for/list ([i (in-range n-workers)])
-    (place-channel-put put-ch i))
-  (eprintf "....~a ~n" 42)
-  (for ([w (in-list workers)])
-    (place-channel-put put-ch 'over))
-  (eprintf "....~a ~n" 24)
-  (for ([w (in-list workers)])
-    (place-wait w)))
+      (if (equal? a 'over)
+          (place-channel-put aa (list success failure err))
+          (let ()
+            (define results (run-test (dynamic-require (build-path src-dir a) 'tests)))
+            (for/fold ([success success]
+                       [failure failure]
+                       [err err]
+                       #:result (loop success failure err))
+                      ([r (in-list results)])
+              (display-test-result r #:suite-names (list (path->string (build-path src-dir a))))
+              (cond
+                [(test-success? r) (values (add1 success) failure err)]
+                [(test-failure? r) (values success (add1 failure) err)]
+                [else (values success failure (add1 err))])))))))
 
 (define unit-tests (list
                     "typecheck-tests.rkt"
@@ -97,7 +60,8 @@
                     "prefab-tests.rkt"
                     "json-tests.rkt"
                     "typed-units-tests.rkt"
-                    "type-constr-tests.rkt"))
+                    "type-constr-tests.rkt"
+                    ))
 (define (prun2 p)
   (define-values (put-ch get-ch) (place-channel))
   (define n-workers p)
@@ -106,8 +70,16 @@
     (place-channel-put put-ch i))
   (for ([w (in-list workers)])
     (place-channel-put put-ch 'over))
-  (for ([w (in-list workers)])
-    (place-wait w)))
+
+  (for/fold ([success 0]
+             [failure 0]
+             [err 0]
+             #:result (printf "success (~a), failure (~a), errror (~a)~n" success failure err))
+            ([w (in-list workers)])
+    (match-define (list s f e) (place-channel-get w))
+    (values (+ s success)
+            (+ f failure)
+            (+ e err))))
 (require racket/cmdline)
 (module+ main
   (command-line #:args (p)
